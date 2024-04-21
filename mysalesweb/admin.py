7bb23262ef4e5f6a6ce935677fe2a8800c6f1419
Chao Wang <chao.wang@exxcellent.de>
2024-04-21 19:19:31 +0200

    feat(admin.py): register all models

--- mysalesweb/admin.py
+++ mysalesweb/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from .Entities import product
+from .Entities import address
+from .Entities import customer
+from .Entities import productInfo
+from .Entities import provider
 
 # Register your models here.
-from .models import Product
-admin.site.register(Product)
+admin.site.register(product.Product)
+admin.site.register(address.Address)
+admin.site.register(customer.Customer)
+admin.site.register(productInfo.ProductInfo)
+admin.site.register(provider.Provider)
