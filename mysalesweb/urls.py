be15e3d6dcf9b319d5fdd14259d3ae00d97533f4
ClaytonStudent <chen.shaohang2046@gmail.com>
2024-04-21 18:47:29 +0200

    Update form and searchSeclet

--- mysalesweb/urls.py
+++ mysalesweb/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("purchase/",view=views.purchase,name="purchase"),
     path("purchaseAdd/",view=views.purchaseAdd,name="purchaseAdd"),
     path("customer/",view=views.customer,name="customer"),
+    path("searchAddSelect/",view=views.searchAddSelect,name="searchAddSelect"),
 ]
--- mysalesweb/views.py
+++ mysalesweb/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 
+from .forms import ProductAddForm
+from .models import Product
+from django.contrib import messages
+
 def index(request):
     return render(request, 'mysalesweb/index.html')
 
@@ -9,10 +13,22 @@ def dashboard(request):
     return render(request, 'mysalesweb/dashboard.html')
 
 def product(request):
-    return render(request, 'mysalesweb/product.html')
+    products = Product.objects.all()
+    return render(request, 'mysalesweb/product.html', {'products': products})
 
 def productAdd(request):
-    return render(request, 'mysalesweb/productAdd.html')
+    if request.method == 'POST':
+        form = ProductAddForm(request.POST, request.FILES)
+        print(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product')
+        else:
+            messages.error(request, 'Form is invalid')
+    else:     
+        form = ProductAddForm()
+    return render(request, 'mysalesweb/productAdd.html', {'form': form})
+
 
 def productEdit(request):
     return render(request, 'mysalesweb/productEdit.html')
@@ -25,3 +41,6 @@ def purchaseAdd(request):
 
 def customer(request):
     return render(request, 'mysalesweb/customer.html')
+
+def searchAddSelect(request):
+    return render(request, 'mysalesweb/searchAddSelect.html')
